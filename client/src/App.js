e945cb31c0c90cf3b7d5c4f7d6c0620ca3c3e0ca
Maky <franklinjaradev@gmail.com>
2021-05-21 23:38:45 -0400

    Added redux, actions and reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
 import memories from "./images/memories.png";
 import useStyles from "./styles";
+import { getPosts } from "./actions/postsActions";
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxwidth="lg">
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,4 +2,4 @@ import axios from "axios";
 
 const url = "http://localhost:5000/api/posts";
 
-export const fetchPost = () => axios.get(url);
+export const fetchPosts = () => axios.get(url);
