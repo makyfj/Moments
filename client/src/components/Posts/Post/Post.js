5e0fdde4425512951275e4be9a241f1c67bada86
Maky <franklinjaradev@gmail.com>
2021-05-23 17:07:38 -0400

    Like functionality added

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -12,7 +12,7 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import { useDispatch } from "react-redux";
-import { deletePost } from "../../../actions/postsActions";
+import { deletePost, likePost } from "../../../actions/postsActions";
 
 import useStyles from "./styles";
 
@@ -56,7 +56,11 @@ const Post = ({ post, setCurrentId }) => {
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(likePost(post._id))}
+        >
           <ThumbUpAltIcon fontSize="small" />
           Like {post.likeCount}
         </Button>
